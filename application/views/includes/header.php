e743fed29c05f3390e48a50dad1ba0ac4398b35a
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2020-12-08 19:47:55 -0300

    Updates

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,13 +1,13 @@
 <header class='header-top text-center margin-bt20'>
     <img class='logo' src='<?= DESIGN_PATH ?>img/logo.png'/><h1 class='text-light'>Rita's Cookies</h1>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Painel</a>
+  <a class="navbar-brand" href="<?= BASE_URL?>">Painel</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Inicial <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link active" href="<?= BASE_URL?>">Inicial <span class="sr-only">(current)</span></a>
       <a class="nav-item nav-link" href="<?= BASE_URL.'clientes' ?>">Clientes</a>
       <a class="nav-item nav-link" href="<?= BASE_URL.'pedidos' ?>">Pedidos</a>
       <a class="nav-item nav-link " href="<?= BASE_URL.'Relatorio' ?>">Relatório</a>
--- application/views/pages/landpage.php
+++ application/views/pages/landpage.php
@@ -1,14 +1,24 @@
 
 <div class="container">
   <div class="row">
-    <div class="col-md-12 text-center">
-      <h1>Lista de cliente</h1>
+    <div class="col-md-12 text-center mb-4">
+      <h1>Dashboard</h1>
     </div>
-    <div class="col-md-6 text-center">
-    <a class="nav-item nav-link" href="<?= BASE_URL.'clientes' ?>">Clientes</a>
+    <div class="col-md-3 text-center"></div>
+    <div class="col-md-2 text-center">
+      <a class="nav-item nav-link" href="<?= BASE_URL.'clientes' ?>">
+      <img src='<?= DESIGN_PATH ?>img/cliente.png' style='max-width: 100px;'/>
+      <h4 class='text-dark'>Clientes</h4></a>
     </div>
-    <div class="col-md-6 text-center">
-    <a class="nav-item nav-link" href="<?= BASE_URL.'pedidos' ?>">Pedidos</a>
+    <div class="col-md-2 text-center">
+      <a class="nav-item nav-link" href="<?= BASE_URL.'pedidos' ?>">
+      <img src='<?= DESIGN_PATH ?>img/pedidos.png'  style='max-width: 100px;'/>
+      <h4 class='text-dark'>Pedidos</h4></a>
+    </div>
+    <div class="col-md-2 text-center">
+      <a class="nav-item nav-link" href="<?= BASE_URL.'relatorio' ?>">
+      <img src='<?= DESIGN_PATH ?>img/relatorio.png'  style='max-width: 100px;'/>
+      <h4 class='text-dark'>Relatorios</h4></a>
     </div>
   </div>
 </div>
